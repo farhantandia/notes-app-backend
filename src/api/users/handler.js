f294d04e12cc31d1ebfcc0d785f9e146deed52a9
M. Farhan Tandia <farhantandia@gmail.com>
2021-08-15 19:23:02 +0800

    correct v3

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -1,20 +1,21 @@
 const ClientError = require('../../exceptions/ClientError');
- 
+
 class UsersHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
-    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+
     this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
   }
- 
+
   async postUserHandler(request, h) {
     try {
       this._validator.validateUserPayload(request.payload);
       const { username, password, fullname } = request.payload;
- 
+
       const userId = await this._service.addUser({ username, password, fullname });
- 
+
       const response = h.response({
         status: 'success',
         message: 'User berhasil ditambahkan',
@@ -33,7 +34,7 @@ class UsersHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -44,12 +45,11 @@ class UsersHandler {
       return response;
     }
   }
+
   async getUserByIdHandler(request, h) {
     try {
       const { id } = request.params;
- 
       const user = await this._service.getUserById(id);
- 
       return {
         status: 'success',
         data: {
@@ -65,8 +65,7 @@ class UsersHandler {
         response.code(error.statusCode);
         return response;
       }
- 
- 
+
       // server ERROR!
       const response = h.response({
         status: 'error',
@@ -78,4 +77,5 @@ class UsersHandler {
     }
   }
 }
+
 module.exports = UsersHandler;
